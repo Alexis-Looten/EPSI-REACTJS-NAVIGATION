8a7238c5eeb8ddb655f5b0bf4ec82e1eff0e65b1
alexis.looten <alexis.looten@pingflow.fr>
2023-01-14 14:36:37 +0100

    Try callback

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,14 @@ import "./../styles/SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 
-function SearchBar({ placeholder, data }) {
+function SearchBar(props) {
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
 
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
+    const newFilter = props.data.filter((value) => {
       return value.name.toLowerCase().includes(searchWord.toLowerCase());
     });
 
@@ -21,17 +21,21 @@ function SearchBar({ placeholder, data }) {
     }
   };
 
-  const clearInput = () => {
+  const clearInput = (event) => {
     setFilteredData([]);
     setWordEntered("");
   };
+
+  const handleClick = (value) => {
+    props.reg(value.name);
+  }
 
   return (
     <div className="search">
       <div className="searchInputs">
         <input
           type="text"
-          placeholder={placeholder}
+          placeholder={props.placeholder}
           value={wordEntered}
           onChange={handleFilter}
         />
@@ -45,11 +49,15 @@ function SearchBar({ placeholder, data }) {
       </div>
       {filteredData.length !== 0 && (
         <div className="dataResult">
-          {filteredData.slice(0, 15).map((value, key) => {
+          {filteredData.slice(0, 20).map((value, key) => {
             return (
-              <a className="dataItem" href={value.name} target="_blank" rel="noreferrer">
-                <p>{value.name}</p>
-              </a>
+              // <>
+              //   <button>{value.name}</button>
+                    <button id="ChoiceBtn" className="dataItem"  onClick={handleClick(value)}>{value.name}</button>
+              // </> 
+              // <a className="dataItem" href={value.name} target="_blank" rel="noreferrer">
+              //   <p>{value.name}</p>
+              // </a>
             );
           })}
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
-import Ville from "../json/Ville.json";
+import Region from '../json/Region.json';
+import Departement from '../json/Departement.json';
+import Ville from '../json/Ville.json';
 
 function Home(props){
+    const [places, setPlaces] = useState(Region);
+    const [placeholder, setPlaceholder] = useState("Entrer une région...");
+
+    let reg = "";
+    const dep = "";
+    const vil = "";
+
+    const handleReg = (data) => {
+        reg = data;
+        console.log("reg : "+reg);
+    }
+
+    const handlePlaces = () => {
+
+        if (reg === "") {
+            setPlaces(Region);
+            return
+        } 
+
+        if (dep === "") {
+            setPlaces(Departement);
+            return
+        }
+
+        if (vil === "") {
+            setPlaces(Ville);
+            return
+        }
+        
+    }
+
     return(
         <>
-        <SearchBar placeholder="Entrer une région..." data={Ville} />
+        <SearchBar placeholder={placeholder} data={places} reg={handleReg}/> 
         </>
     );
 }
