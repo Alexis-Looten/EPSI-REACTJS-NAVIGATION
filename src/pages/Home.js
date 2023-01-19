7c215fa295283b04f79503bf0a185a688531869f
alexis.looten <alexis.looten@pingflow.fr>
2023-01-19 21:24:14 +0100

    add meteo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import MapComponent from "../components/Mapping";
 import Local from "../components/Local"
+import MeteoComp from "../components/Meteo";
 
 function Home(props){
     const [StartCity, setStartCity] = useState();
@@ -27,11 +28,29 @@ function Home(props){
     return(
         <>
         {/* Start */}
-        <SearchBar selectedCity={handleStart}/>
-        <Local geoloc={handleLocal}/>
+        <fieldset className="">
+          <legend>
+              Lieu de départ
+          </legend>
+          <SearchBar selectedCity={handleStart}/>
+          <Local geoloc={handleLocal}/>
+          <label>
+            Météo :
+            {StartCity && <MeteoComp gps_lat={StartCity.gps_lat} gps_lng={ StartCity.gps_lng}/>}
+          </label>
+        </fieldset>
 
         {/* End */}
-        <SearchBar selectedCity={handleEnd}/>
+        <fieldset className="">
+          <legend>
+              Lieu de d'arrivée
+          </legend>
+          <SearchBar selectedCity={handleEnd}/>
+          <label>
+            Météo :
+            {EndCity && <MeteoComp gps_lat={EndCity.gps_lat} gps_lng={EndCity.gps_lng}/>}
+          </label>
+        </fieldset>
 
         {StartCity && EndCity && <MapComponent start={[StartCity.gps_lat, StartCity.gps_lng]} end={[EndCity.gps_lat, EndCity.gps_lng]}/>}
         </>
