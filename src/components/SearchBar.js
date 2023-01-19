6dfd2229b1a82e0439449cb9de0d9b5c4b3e7ffd
alexis.looten <alexis.looten@pingflow.fr>
2023-01-19 16:56:32 +0100

    optimize SearchBar & Home

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,26 @@ import React, { useState } from "react";
 import "../styles/SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
+import Region from '../json/Region.json';
+import Departement from '../json/Departement.json';
+import Ville from '../json/Ville.json';
 
 function SearchBar(props) {
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
 
+  const [places, setPlaces] = useState(Region);
+  const [placeholder, setPlaceholder] = useState("Entrer une région...");
+  const [type, setType] = useState("region");
+
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [selectedDepartment, setSelectedDepartment] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = props.data.filter((value) => {
+    const newFilter = places.filter((value) => {
       return value.name.toLowerCase().includes(searchWord.toLowerCase());
     });
 
@@ -27,16 +38,30 @@ function SearchBar(props) {
   };
 
   const handleClick = (value) => {
-    clearInput()
-    props.selectedValue(value);
+    clearInput();
+    if (type === 'region') {
+      setSelectedRegion(value.name); 
+      setPlaces(Departement.filter(item => item.region_code === value.code));
+      setType("department")
+      setPlaceholder("Entrer un département...")
+    } else if (type === 'department') {
+      setSelectedDepartment(value.name);
+      setPlaces(Ville.filter(item => item.department_code === value.code))
+      setType("city")
+      setPlaceholder("Entrer une ville...")
+    } else if (type === 'city') {
+      setSelectedCity(value.name);
+      props.selectedCity(value);
+    }
   }
 
   return (
+    <>
     <div className="search">
       <div className="searchInputs">
         <input
           type="text"
-          placeholder={props.placeholder}
+          placeholder={placeholder}
           value={wordEntered}
           onChange={handleFilter}
         />
@@ -50,20 +75,21 @@ function SearchBar(props) {
       </div>
       {filteredData.length !== 0 && (
         <div className="dataResult">
-          {filteredData.slice(0, 20).map((value, key) => {
+          {filteredData.slice(0, 100).map((value, key) => {
             return (
-              // <>
-              //   <button>{value.name}</button>
                     <button key={value.id} id="ChoiceBtn" className="dataItem"  onClick={() => handleClick(value)}>{value.name} ({value.code}{value.zip_code})</button>
-              // </> 
-              // <a className="dataItem" href={value.name} target="_blank" rel="noreferrer">
-              //   <p>{value.name}</p>
-              // </a>
             );
           })}
         </div>
       )}
     </div>
+
+    <div className="Choices">
+      <p>Région choisie : {selectedRegion}</p>
+      <p>Département choisie : {selectedDepartment}</p>
+      <p>Ville choisie : {selectedCity}</p>
+    </div>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,86 +1,32 @@
 import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
-import Region from '../json/Region.json';
-import Departement from '../json/Departement.json';
-import Ville from '../json/Ville.json';
 import MapComponent from "../components/Mapping";
 
 function Home(props){
-    const [places, setPlaces] = useState(Region);
-    const [placeholder, setPlaceholder] = useState("Entrer une région...");
-    const [type, setType] = useState("region");
+    const [StartCity, setStartCity] = useState();
 
-    const [selectedRegion, setSelectedRegion] = useState('');
-    const [selectedDepartment, setSelectedDepartment] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
-
-    const [startPlaces, setStartPlaces] = useState(Region);
-    const [startPlaceholder, setStartPlaceholder] = useState("Entrer une région...");
-    const [startType, setStartType] = useState("region");
-
-    const [selectedStartRegion, setSelectedStartRegion] = useState('');
-    const [selectedStartDepartment, setSelectedStartDepartment] = useState('');
-    const [selectedStartCity, setSelectedStartCity] = useState('');
-
-    const handleEnd = (selectedEnd) => {
-        // console.log(selectedValue, type)
-        if (type === 'region') {
-            setSelectedRegion(selectedEnd.name); 
-            setPlaces(Departement.filter(item => item.region_code === selectedEnd.code));
-            setType("department")
-            setPlaceholder("Entrer un département...")
-          } else if (type === 'department') {
-            setSelectedDepartment(selectedEnd.name);
-            setPlaces(Ville.filter(item => item.department_code === selectedEnd.code))
-            setType("city")
-            setPlaceholder("Entrer une ville...")
-          } else if (type === 'city') {
-            setSelectedCity(selectedEnd);
-          }
+    const handleStart = (city) => {
+      console.log(city)
+      setStartCity(city);
     }
 
-    const handleStart = (selectedStart) => {
-      // console.log(selectedValue, type)
-      if (startType === 'region') {
-        setSelectedStartRegion(selectedStart.name); 
-          setStartPlaces(Departement.filter(item => item.region_code === selectedStart.code));
-          setStartType("department")
-          setStartPlaceholder("Entrer un département...")
-        } else if (startType === 'department') {
-          setSelectedStartDepartment(selectedStart.name);
-          setStartPlaces(Ville.filter(item => item.department_code === selectedStart.code))
-          setStartType("city")
-          setStartPlaceholder("Entrer une ville...")
-        } else if (startType === 'city') {
-          setSelectedStartCity(selectedStart);
-        }
-  }
+    const [EndCity, setEndCity] = useState();
+    
+    const handleEnd = (city) => {
+      console.log(city)
+      setEndCity(city);
+    }
+    
 
     return(
         <>
-
         {/* Start */}
-        <SearchBar placeholder={startPlaceholder} data={startPlaces} selectedValue={handleStart}/>
-        <div className="Choices">
-            <p>Région choisie : {selectedStartRegion}</p>
-            <p>Département choisie : {selectedStartDepartment}</p>
-            <p>Ville choisie : {selectedStartCity.name}</p>
-            
-            
-        </div>
+        <SearchBar selectedCity={handleStart}/>
 
         {/* End */}
-        <SearchBar placeholder={placeholder} data={places} selectedValue={handleEnd}/>
-        <div className="Choices">
-            <p>Région choisie : {selectedRegion}</p>
-            <p>Département choisie : {selectedDepartment}</p>
-            <p>Ville choisie : {selectedCity.name}</p>
-        </div>
+        <SearchBar selectedCity={handleEnd}/>
 
-
-        {selectedCity && selectedStartCity && <MapComponent start={[selectedStartCity.gps_lat, selectedStartCity.gps_lng]} end={[selectedCity.gps_lat, selectedCity.gps_lng]}/>}
-
-        
+        {StartCity && EndCity && <MapComponent start={[StartCity.gps_lat, StartCity.gps_lng]} end={[EndCity.gps_lat, EndCity.gps_lng]}/>}
         </>
     );
 }
